d00c2c69752bb44209d2086b01a03c1c14fffd54
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-08-23 02:32:35 +0800

    change testimonials to screenshots

--- src/_component/_testimonial/TestimonialSection.js
+++ src/_component/_testimonial/TestimonialSection.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import Image from 'next/image';
 
 import Testimonial from './Testimonial';
 
@@ -9,9 +10,15 @@ export default function TestimonialSection(props) {
     <TestimonialSectionStyled id={props.id} className={props.className}>
       <h2>Testimonials</h2>
       <div className="testimonials">
+        <div>
+          <Image src={"/testimonials/testimonial1.jpeg"} alt="Review from Vijay" width={473} height={1024} priority={true}/>
+        </div>
+        <div>
+          <Image src={"/testimonials/testimonial2.jpeg"} alt="Review from Kelvin" width={473} height={1024} priority={true}/>
+        </div>
+        {/* <Testimonial/>
         <Testimonial/>
-        <Testimonial/>
-        <Testimonial/>
+        <Testimonial/> */}
       </div>
     </TestimonialSectionStyled>
   );
@@ -25,6 +32,11 @@ function createTestimonialSectionStyled() {
 
       @media (min-width: 768px) {
         grid-template-columns: repeat( auto-fit, minmax(300px, 1fr));
+      }
+
+      img {
+        width: 100%;
+        height: auto;
       }
     }
   `
